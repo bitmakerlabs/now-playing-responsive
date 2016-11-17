e7016c92a6f9d83c2884db2debfa7ec68635ce12
Mina Mikhail <mina@fightingtheboss.com>
2016-11-17 15:44:01 -0500

    Add methods for fetching Movie poster and backdrop

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,23 @@
 class Movie < ApplicationRecord
+  IMAGE_CONFIG = {
+    base_url: 'http://image.tmdb.org/t/p/',
+    poster_sizes: ["w92", "w154", "w185", "w342", "w500", "w780", "original"],
+    backdrop_sizes: ["w300", "w780", "w1280", "original"]
+  }
+
+  def poster(size = "w780")
+    if poster_path
+      IMAGE_CONFIG[:base_url] + size + poster_path
+    else
+      'http://placehold.it/780x1170/?text="Poster not available"'
+    end
+  end
+
+  def backdrop(size = "w1280")
+    if backdrop_path
+      IMAGE_CONFIG[:base_url] + size + backdrop_path
+    else
+      'http://placehold.it/1280x720/?text=Backdrop not available'
+    end
+  end
 end
